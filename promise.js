96c9161546c0ac2c5e571f79f3e0c6c078d62e1b
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-08 20:51:42 +0600

    promise understud

--- promise.js
+++ promise.js
@@ -1,1 +1,12 @@
-console.log('promise');
+const getData = new Promise((resolve, reject) => {   
+    const num=Math.random()*10
+    if (num < 5) {
+        resolve(num)
+    } else {
+        reject('No Data Found')
+    }
+})
+
+getData
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
